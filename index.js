76632efd127855f47097848bf702ac67438507a3
Harshil Goel <harshilgoel96@gmail.com>
2020-09-11 21:09:33 +0530

    Axios Package Installed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersRouter from "./routes/users.js";
+import axios from "axios";
 
 //Initialise app variable
 const app = express();
